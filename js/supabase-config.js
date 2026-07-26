8df11742b984924b64d1b6e2c70635a3e2fea1ff
Miguel Custódio <153029138+MiguelCustodio9@users.noreply.github.com>
2026-07-26 23:45:20 +0100

    Update Supabase anon key in configuration file

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -3,6 +3,6 @@
 // Substitui pelos dados do teu projeto (Supabase Studio > Project Settings > API)
 // =========================================================
 const SUPABASE_URL = "https://mfsywwojegtyyofzpvca.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_OaTYRFWZG7cici5Rnxe20A_7zxUAFqd";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mc3l3d29qZWd0eXlvZnpwdmNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwOTU3MzAsImV4cCI6MjEwMDY3MTczMH0.y5UJ8_hdKcOVScqttTUpdeVOMfipWy280CYwiaCWUiA";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
